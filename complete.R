65ddb63a6339fc22f19a5bdad99f31f3c72ca246
ShahmatuN <ShahmatuN@users.noreply.github.com>
2015-08-07 09:18:32 +0300

    fix

--- complete.R
+++ complete.R
@@ -13,23 +13,25 @@ complete <- function(directory, id = 1:332) {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   k <- 1
-  nobs<-matrix(,length(id),2)
+  
     if(grep("specdata", directory) == 1) 
   {
     directory <- ("./specdata/")
-  }
+    }
+  fullData <- id
   allFiles <- as.character( list.files(directory) )
   filePaths <- paste(directory, allFiles, sep="")
   for(i in id) 
   {
     currentFile <- read.csv(filePaths[i], header=T, sep=",")
-    head(currentFile)
-    naRemoved <- currentFile[!is.na(currentFile[, "sulfate"]), "sulfate"]
-    nobs[k,1]<- i
-    nobs[k,2] <-length(naRemoved)
+    
+    fullData[k] <- sum(complete.cases(currentFile))
     k <-k+1
   }
+nobs <- data.frame(id = id, nobs = fullData)
 return(nobs)
 }
+complete("specdata", 1)
 complete("specdata", c(2, 4, 8, 10, 12))
-
+complete("specdata", 30:25)
+complete("specdata", 3)
